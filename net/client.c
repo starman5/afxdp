b23acc14b6a646eaa2de906dd28e8d984cc6cec2
Matt Kiley <mdkiley@college.harvard.edu>
2023-07-06 06:52:15 -0400

    more

--- net/client.c
+++ net/client.c
@@ -67,7 +67,7 @@ void *send_message(void* arg) {
 
     // Loop to send many SET messages and receive responses
     char buffer[BUFFER_SZ];
-    for (int i = 0; i < 1000; ++i) {
+    for (int i = 0; i < 100000; ++i) {
         uint64_t key = rand();  // random key.  This is probably ideal for minimizing hash collisions
         memset(buffer, '\0', BUFFER_SZ);
         serialize(SET, key, "hello", buffer);
