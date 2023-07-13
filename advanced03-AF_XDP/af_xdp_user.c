b0cfaeed04367cc833f8ea9b1f0764ef4b124b24
Matt Kiley <mdkiley@college.harvard.edu>
2023-07-13 15:06:17 -0400

    t

--- advanced03-AF_XDP/af_xdp_user.c
+++ advanced03-AF_XDP/af_xdp_user.c
@@ -277,6 +277,7 @@ static void complete_tx(struct xsk_socket_info *xsk)
 					XSK_RING_CONS__DEFAULT_NUM_DESCS,
 					&idx_cq);
 
+	printf("completed: %d\n", completed);
 	if (completed > 0) {
 		for (int i = 0; i < completed; i++)
 			xsk_free_umem_frame(xsk,
@@ -318,9 +319,9 @@ static bool process_packet(struct xsk_socket_info *xsk,
 
 	*/
 
-	//printf("Received packet\n");
+	printf("Received packet\n");
 
-	/*if (false) {
+	if (false) {
 		int ret;
 		uint32_t tx_idx = 0;
 		uint8_t tmp_mac[ETH_ALEN];
@@ -347,16 +348,17 @@ static bool process_packet(struct xsk_socket_info *xsk,
 
 		csum_replace2(&icmp->icmp6_cksum,
 			      htons(ICMPV6_ECHO_REQUEST << 8),
-			      htons(ICMPV6_ECHO_REPLY << 8));*/
+			      htons(ICMPV6_ECHO_REPLY << 8));
 
 		/* Here we sent the packet out of the receive port. Note that
 		 * we allocate one entry and schedule it. Your design would be
 		 * faster if you do batch processing/transmission */
 
-		/*ret = xsk_ring_prod__reserve(&xsk->tx, 1, &tx_idx);
-		if (ret != 1) {*/
+		ret = xsk_ring_prod__reserve(&xsk->tx, 64, &tx_idx);
+		if (ret != 1) {
 			/* No more transmit slots, drop the packet */
-			/*return false;
+			printf("no more transmit slots\n");
+			return false;
 		}
 
 		xsk_ring_prod__tx_desc(&xsk->tx, tx_idx)->addr = addr;
@@ -369,8 +371,7 @@ static bool process_packet(struct xsk_socket_info *xsk,
 		return true;
 	}
 
-	return false;*/
-	return true;
+	return false;
 }
 
 static void handle_receive_packets(struct xsk_socket_info *xsk)
@@ -427,7 +428,7 @@ static void handle_receive_packets(struct xsk_socket_info *xsk)
 	xsk->stats.rx_packets += rcvd;
 
 	/* Do we need to wake up the kernel for transmission */
-	//complete_tx(xsk);
+	complete_tx(xsk);
   }
 
 //static void rx_and_process(struct config* cfg,
