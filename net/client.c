07c3f44d75e32cbcecdd6337fcf48e36f8a410d4
Matt Kiley <mdkiley@college.harvard.edu>
2023-08-03 03:41:35 -0400

    accomodate

--- net/client.c
+++ net/client.c
@@ -16,7 +16,7 @@ UDP client, meant to stress the server, designed to measure throughput
 #define SERVER_IP "10.10.2.1"   // Change this to the ip address of the server
 #define CLIENT_IP "10.10.2.2"
 #define SERVER_PORT 8889
-#define NUM_CORES 100
+#define NUM_CORES 10000
 #define TABLE_SIZE  10000
 
 // Commands to serialize
