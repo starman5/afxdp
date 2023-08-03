70cd534f7b4f48b9bf3869532b57baf502f6ae11
Matt Kiley <mdkiley@college.harvard.edu>
2023-08-03 11:55:37 -0400

    testing

--- net/follower.c
+++ net/follower.c
@@ -20,8 +20,8 @@ UDP client, meant to stress the server, designed to measure latency
 
 #define SERVER_PORT 8889
 #define COMM_PORT   8890
-#define MSG_PER_CORE    100000
-#define TABLE_SIZE  50000
+#define MSG_PER_CORE    500000
+#define TABLE_SIZE  10000
 
 // Commands to serialize
 #define NON     5
