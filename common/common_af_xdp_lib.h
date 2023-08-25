ddd1cb997aa1ec57eb820f20dc502fe94713667a
Matt Kiley <mdkiley@college.harvard.edu>
2023-08-25 19:03:37 -0400

    moving static vars

--- common/common_af_xdp_lib.h
+++ common/common_af_xdp_lib.h
@@ -139,52 +139,6 @@ int pin_thread_to_core(int core_id);
 
 __u32 xsk_ring_prod__free(struct xsk_ring_prod* r);
 
-static const char* __doc__ = "AF_XDP kernel bypass example\n";
-
-static const struct option_wrapper long_options[] = {
-
-    {{"help", no_argument, NULL, 'h'}, "Show help", false},
-
-    {{"dev", required_argument, NULL, 'd'},
-     "Operate on device <ifname>",
-     "<ifname>",
-     true},
-
-    {{"skb-mode", no_argument, NULL, 'S'},
-     "Install XDP program in SKB (AKA generic) mode"},
-
-    {{"native-mode", no_argument, NULL, 'N'},
-     "Install XDP program in native mode"},
-
-    {{"auto-mode", no_argument, NULL, 'A'}, "Auto-detect SKB or native mode"},
-
-    {{"force", no_argument, NULL, 'F'},
-     "Force install, replacing existing program on interface"},
-
-    {{"copy", no_argument, NULL, 'c'}, "Force copy mode"},
-
-    {{"zero-copy", no_argument, NULL, 'z'}, "Force zero-copy mode"},
-
-    {{"queue", required_argument, NULL, 'Q'},
-     "Configure interface receive queue for AF_XDP, default=0"},
-
-    {{"poll-mode", no_argument, NULL, 'p'},
-     "Use the poll() API waiting for packets to arrive"},
-
-    {{"quiet", no_argument, NULL, 'q'}, "Quiet mode (no output)"},
-
-    {{"filename", required_argument, NULL, 1},
-     "Load program from <file>",
-     "<file>"},
-
-    {{"progname", required_argument, NULL, 2},
-     "Load program from function <name> in the ELF file",
-     "<name>"},
-
-    {{0, 0, NULL, 0}, NULL, false}};
-
-static bool global_exit;
-
 struct xsk_umem_info* configure_xsk_umem(void* buffer, uint64_t size);
 
 uint64_t xsk_alloc_umem_frame(struct xsk_socket_info* xsk);
