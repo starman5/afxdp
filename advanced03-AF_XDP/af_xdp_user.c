b3f130b16cc62036af6cca085cab680fe92e9cb1
Matt Kiley <mdkiley@college.harvard.edu>
2023-08-26 04:23:30 -0400

    refactor

--- advanced03-AF_XDP/af_xdp_user.c
+++ advanced03-AF_XDP/af_xdp_user.c
@@ -117,7 +117,7 @@ bool custom_processing(uint8_t* pkt, HASHTABLE_T hashtable, Spinlock* locks, Cou
 }
 
 int main(int argc, char** argv) {
-  int init_success = init_afxdp(int argc, char** argv);
+  int init_success = init_afxdp(argc, argv);
   if (init_success != 0) {
     perror("FAIL");
   }
